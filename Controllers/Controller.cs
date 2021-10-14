8e2e9c8936d4e346c3075e0eb0def99a01763444
claraherz <89048659+claraherz@users.noreply.github.com>
2021-10-14 17:17:02 -0300

    Add files via upload

---
+++ Controllers/Controller.cs
@@ -0,0 +1,36 @@
+﻿using check_health.Controllers;
+using check_health.Models.ModelCadastroeLogin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check_health.Controllers
+{
+    class Controller
+    {
+        public static void CadastroDados(string a, string b, string c)
+        {
+            UsuarioCadastro user = new UsuarioCadastro();
+
+            user.Nome = a;
+            user.Email = b;
+            user.Senha = c;
+
+            UsuarioCadastroController.CheckerInsert(user);
+        }
+
+        public static void LoginDados(string a, string b)
+        {
+            UsuarioLogin user = new UsuarioLogin();
+
+            user.Email = a;
+            user.Senha = b;
+
+            UsuarioLoginController.CheckerSelect(a, b);
+        }
+
+
+    }
+}
