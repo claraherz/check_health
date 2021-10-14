3306283bdf0cbb51d7976bc51b39542c8accffca
MatteoMastella <89214125+MatteoMastella@users.noreply.github.com>
2021-10-14 15:05:38 -0300

    Add files via upload

--- Views/Cadastro.cs
+++ Views/Cadastro.cs
@@ -16,5 +16,10 @@ namespace check_health.Views
         {
             InitializeComponent();
         }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
