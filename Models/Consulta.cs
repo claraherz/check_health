6d538b1586062d034466c73b372bb7e2fae8c23b
claraherz <89048659+claraherz@users.noreply.github.com>
2021-10-14 14:46:26 -0300

    Add files via upload

--- Models/Consulta.cs
+++ Models/Consulta.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Check.Health.Models
+namespace check_health.Models
 {
     public class Consulta
     {
