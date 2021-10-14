fc8b3ed65cf2268e495048877df334bd89e99f99
claraherz <stocklermah@outlook.com>
2021-10-14 16:20:28 -0300

    Revert "Delete UsuarioLoginController.cs"
    This reverts commit 5c46a70abfedb6088f2e75c013872abc69154cec.

---
+++ Controllers/UsuarioLoginController.cs
@@ -0,0 +1,42 @@
+﻿using check_health.Models;
+using check_health.Models.ModelCadastroeLogin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace check_health.Controllers
+{
+    public class UsuarioLoginController
+    {
+        public static Response CheckerSelect(string email, string senha, out UsuarioLogin user)
+        {
+            user = new UsuarioLogin();
+
+            if (!string.IsNullOrEmpty(email) && email.Length < 51)
+            {
+                if (!string.IsNullOrEmpty(senha) && senha.Length < 51)
+                {
+                    return UsuarioLoginBD.Select(email, senha, out user);
+                }
+                else
+                {
+                    return new Response
+                    {
+                        Executed = false,
+                        ErrorMessage = "Senha está incorreta"
+                    };
+                }
+            }
+            else
+            {
+                return new Response
+                {
+                    Executed = false,
+                    ErrorMessage = "E-mail está incorreto"
+                };
+            }
+
+        }
+    }
+}
