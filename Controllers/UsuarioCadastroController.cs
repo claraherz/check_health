be11ca8bd4304460f6afb60a912be49307ba2091
MatteoMastella <matteomastella@outlook.com>
2021-10-18 13:09:15 -0300

    18/09

--- Controllers/UsuarioCadastroController.cs
+++ Controllers/UsuarioCadastroController.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using check_health.Models;
+using check_health.Models;
 using check_health.Models.ModelCadastroeLogin;
 using System;
 using System.Collections.Generic;
@@ -49,55 +48,3 @@ namespace check_health.Controllers
         }
     }
 }
-=======
-﻿using check_health.Models;
-using check_health.Models.ModelCadastroeLogin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace check_health.Controllers
-{
-    public class UsuarioCadastroController
-    {
-        public static Response CheckerInsert(UsuarioCadastro user)
-        {
-            if (!string.IsNullOrEmpty(user.Nome) && user.Nome.Length < 51)
-            {
-                if (!string.IsNullOrEmpty(user.Email) && user.Email.Length < 51)
-                {
-                    if (!string.IsNullOrEmpty(user.Senha) && user.Senha.Length < 51)
-                    {
-                        return UsuarioCadastroBD.Insert(user);
-                    }
-                    else
-                    {
-                        return new Response
-                        {
-                            Executed = false,
-                            ErrorMessage = "Senha num padrão incorreto"
-                        };
-                    }
-                }
-                else
-                {
-                    return new Response
-                    {
-                        Executed = false,
-                        ErrorMessage = "E-mail num padrão incorreto"
-                    };
-                }
-            }
-            else
-            {
-                return new Response
-                {
-                    Executed = false,
-                    ErrorMessage = "Nome num padrão incorreto"
-                };
-            }
-        }
-    }
-}
->>>>>>> 01a15d8919aae7db7558a17afe27a15b9a80699c
